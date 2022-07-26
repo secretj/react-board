f528ebe2479455e7a0471cf2189ec2a417811c3e
secretj <qkrwlsgud890@gmail.com>
2022-07-26 21:33:19 +0900

    board 작업 시작 07262133

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { ClassicEditor } from '@ckeditor/ckeditor5-build-classic';
+import { useState } from 'react';
 
 function App() {
+  const [movieContent, setMovieContent] =useState({
+    title: '',
+    content: ''
+  })
+
+  const getValue = e => {
+    const { name, value} = e.target;
+    console.log(name, value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Review</h1>
+      <div className='movie-container'>
+        <h2>제목</h2>
+        <div>
+          내용
+        </div>
+      </div>
+      <div className='from-wrapper'>
+        <input className="title-input"
+                type='text'
+              placeholder='제목'
+              onChange={getValue}
+              name='title'
+          />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+      <button className="submit-button">입력</button>
     </div>
   );
 }
