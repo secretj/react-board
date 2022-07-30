cb928419b958c49227b43e28ab1d97c8fd3f2f9d
secretj <qkrwlsgud890@gmail.com>
2022-07-30 17:18:32 +0900

    0730

--- src/App.js
+++ src/App.js
@@ -1,54 +1,62 @@
-import './App.css';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import { ClassicEditor } from '@ckeditor/ckeditor5-build-classic';
 import { useState } from 'react';
+import './App.css';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import { ClassicEditor } from '@ckeditor/ckeditor5-build-classic';
+// //import ReactHtmlParser from 'react-html-parser';
 
 function App() {
-  const [movieContent, setMovieContent] =useState({
+  const [stateContent, setStateContent] = useState({
     title: '',
     content: ''
   })
 
+  const [viewContent, setViewContent]=useState([]);
+
   const getValue = e => {
-    const { name, value} = e.target;
-    console.log(name, value);
-  }
+    const { name, value } = e.target;
+    setStateContent({
+      ...stateContent,
+      [name]: value
+    })
+    console.log(stateContent);
+  };
+
+
+
   return (
     <div className="App">
-      <h1>Movie Review</h1>
-      <div className='movie-container'>
-        <h2>제목</h2>
-        <div>
-          내용
-        </div>
+      <h1>Williroy homework</h1>
+      <div className='board-container'>
+        {viewContent.map(element =>
+          <div className='sub-board-container'>
+            <h2>{element.title}</h2>
+            <div>
+              {element.content}
+            </div>
+          </div>
+        )}
       </div>
       <div className='from-wrapper'>
         <input className="title-input"
-                type='text'
-              placeholder='제목'
-              onChange={getValue}
-              name='title'
-          />
-        <CKEditor
-          editor={ClassicEditor}
-          data="<p>Hello from CKEditor 5!</p>"
-          onReady={editor => {
-            // You can store the "editor" and use when it is needed.
-            console.log('Editor is ready to use!', editor);
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(event, editor) => {
-            console.log('Blur.', editor);
-          }}
-          onFocus={(event, editor) => {
-            console.log('Focus.', editor);
-          }}
+          type='text'
+          placeholder='제목'
+          onChange={getValue}
+          name='title'
         />
+        <div>
+          <input className="content-input"
+              type='text'
+              placeholder='내용'
+              onChange={getValue}
+              name='content'
+            />
+        </div>
       </div>
-      <button className="submit-button">입력</button>
+      <button className="submit-button" onClick={()=>{
+        alert('게시물을 등록하시겠습니까?')
+        setViewContent(viewContent.concat({...stateContent}));
+       }
+      }>입력</button>
     </div>
   );
 }
