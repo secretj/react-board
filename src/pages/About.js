0d5fc83f7c4045f86048e5fe038cb2045d6c4840
secretj <qkrwlsgud890@gmail.com>
2022-08-06 15:42:12 +0900

    8월 6일 useEffect를 이용해 페이지 전환 시 팝업

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,60 +1,73 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+
+
+
 
 function About() {
+
+    useEffect(() => {
+        alert("Hello, About Page")
+    }, [])
+
     const [stateContent, setStateContent] = useState({
-      title: '',
-      content: ''
+        title: '',
+        content: ''
     })
-  
-    const [viewContent, setViewContent]=useState([]);
-  
+
+
+
+    const [viewContent, setViewContent] = useState([]);
+
     const getValue = e => {
-      const { name, value } = e.target;
-      setStateContent({
-        ...stateContent,
-        [name]: value
-      })
-      console.log(stateContent);
+        const { name, value } = e.target;
+        setStateContent({
+            ...stateContent,
+            [name]: value
+        })
+        console.log(stateContent);
     };
-  
-  
-  
+
+
+
     return (
-      <div className="App">
-        <h1>About</h1>
-        <div className='board-container'>
-          {viewContent.map(element =>
-            <div className='sub-board-container'>
-              <h2>{element.title}</h2>
-              <div>
-                {element.content}
-              </div>
+
+        <div className="App" >
+            <h1>About</h1>
+            <div className='board-container'>
+                {viewContent.map(element =>
+                    <div className='sub-board-container'>
+                        <h2>{element.title}</h2>
+                        <div>
+                            {element.content}
+                        </div>
+                    </div>
+                )}
             </div>
-          )}
+            <div className='from-wrapper'>
+                <input className="title-input"
+                    type='text'
+                    placeholder='제목'
+                    onChange={getValue}
+                    name='title'
+                />
+                <div>
+                    <input className="content-input"
+                        type='text'
+                        placeholder='내용'
+                        onChange={getValue}
+                        name='content'
+                    />
+                </div>
+            </div>
+            <button className="submit-button" onClick={() => {
+                alert('About 게시물을 등록하시겠습니까?')
+                setViewContent(viewContent.concat({ ...stateContent }));
+            }
+            }>입력</button>
         </div>
-        <div className='from-wrapper'>
-          <input className="title-input"
-            type='text'
-            placeholder='제목'
-            onChange={getValue}
-            name='title'
-          />
-          <div>
-            <input className="content-input"
-                type='text'
-                placeholder='내용'
-                onChange={getValue}
-                name='content'
-              />
-          </div>
-        </div>
-        <button className="submit-button" onClick={()=>{
-          alert('About 게시물을 등록하시겠습니까?')
-          setViewContent(viewContent.concat({...stateContent}));
-         }
-        }>입력</button>
-      </div>
+
+
     );
-  }
+}
 
 export default About;
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import '../App.css';
 
 
+// function PageChange(props) {
+//   return alert("Hello, " + props.name + " Page")
+// };
+
+
 function Board() {
+
+  useEffect(()=>{
+    alert("Hello, Board Page")
+  },[])
+
   const [stateContent, setStateContent] = useState({
     title: '',
     content: ''
   })
 
-  const [viewContent, setViewContent]=useState([]);
+  const [viewContent, setViewContent] = useState([]);
 
   const getValue = e => {
     const { name, value } = e.target;
@@ -17,45 +27,54 @@ function Board() {
       [name]: value
     })
     console.log(stateContent);
+
+  
   };
 
 
 
   return (
-    <div className="App">
-      <h1>Williroy Board</h1>
-      <div className='board-container'>
-        {viewContent.map(element =>
-          <div className='sub-board-container'>
-            <h2>{element.title}</h2>
+    <>
+
+
+      <div className="App">
+        <h1>Williroy Board</h1>
+        <div className='board-container'>
+          {viewContent.map(element =>
+            <div className='sub-board-container'>
+              <h2>{element.title}</h2>
+              <div>
+                {element.content}
+              </div>
+            </div>
+          )}
+        </div>
+        <form >
+          <div className='from-wrapper'>
+            <input className="title-input"
+              type='text'
+              placeholder='제목'
+              onChange={getValue}
+              name='title'
+            />
             <div>
-              {element.content}
+              <input className="content-input"
+                type='text'
+                placeholder='내용'
+                onChange={getValue}
+                name='content'
+              />
             </div>
           </div>
-        )}
+          <button className="submit-button" type="submit" onClick={() => {
+            alert('게시물을 등록하시겠습니까?')
+            setViewContent(viewContent.concat({ ...stateContent }));
+          }
+          }>입력</button>
+        </form>
       </div>
-      <div className='from-wrapper'>
-        <input className="title-input"
-          type='text'
-          placeholder='제목'
-          onChange={getValue}
-          name='title'
-        />
-        <div>
-          <input className="content-input"
-              type='text'
-              placeholder='내용'
-              onChange={getValue}
-              name='content'
-            />
-        </div>
-      </div>
-      <button className="submit-button" onClick={()=>{
-        alert('게시물을 등록하시겠습니까?')
-        setViewContent(viewContent.concat({...stateContent}));
-       }
-      }>입력</button>
-    </div>
+
+    </>
   );
 }
 
